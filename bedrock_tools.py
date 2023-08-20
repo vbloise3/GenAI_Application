f1da0bffb0fdac4edd7e32f8b795f64541c1b3f9
Vincent Bloise <vtbloise@amazon.com>
2023-08-20 08:48:26 -0400

    added multilingual support

--- bedrock_tools.py
+++ bedrock_tools.py
@@ -4,6 +4,11 @@ import boto3
 from langchain.embeddings import BedrockEmbeddings
 from langchain.vectorstores import FAISS
 from transformers import Tool
+import os
+import pinecone
+import ai21
+
+ai21.api_key = os.environ.get('AI21_API_KEY')
 
 bedrock = boto3.client(
     service_name="bedrock",
@@ -11,7 +16,26 @@ bedrock = boto3.client(
     endpoint_url="https://bedrock.us-east-1.amazonaws.com",
 )
 
-import os
+# get api key from app.pinecone.io
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY') 
+# find your environment next to the api key in pinecone console
+PINECONE_ENV = os.environ.get('PINECONE_ENVIRONMENT') 
+
+pinecone.init(
+    api_key=PINECONE_API_KEY,
+    environment=PINECONE_ENV
+)
+
+def call_ai21(prompt):
+    response = ai21.Completion.execute(
+    model='j1-large',
+    prompt=prompt,
+    temperature=0.65,
+    minTokens=4,
+    maxTokens=32,
+    numResults=1
+)
+    return response
 
 def call_bedrock(prompt):
     prompt_config = {
@@ -26,7 +50,7 @@ def call_bedrock(prompt):
 
     body = json.dumps(prompt_config)
 
-    modelId = "amazon.titan-tg1-large"
+    modelId = "amazon.titan-tg1-large" #"ai21.j2-ultra" 
     accept = "application/json"
     contentType = "application/json"
 
@@ -45,12 +69,12 @@ class AWSWellArchTool(Tool):
     inputs = ["text"]
     outputs = ["text"]
 
-    def __call__(self, query):
+    def __call__(self, query, translation_language):
         # Find docs
         embeddings = BedrockEmbeddings()
         vectorstore = FAISS.load_local("local_index", embeddings)
         docs = vectorstore.similarity_search(query)
-        
+                
         context = ""
 
         doc_sources_string = ""
@@ -64,23 +88,37 @@ class AWSWellArchTool(Tool):
 
         Question: {query}
         Answer:"""
+        # print prompt for log
+        print("prompt:\n")
+        print(prompt)
+        print("\nend of prompt\n")
 
         generated_text = call_bedrock(prompt)
-        print(generated_text)
-
-        resp_json = {"ans": str(generated_text), "docs": doc_sources_string}
+        #print(generated_text)
+        #translate
+        if translation_language:
+            print("translating\n")
+            translation_prompt = generated_text 
+            try:
+                translated_text = call_ai21(translation_prompt)
+                resp_json = {"ans": str(translated_text['prompt']['text']), "docs": doc_sources_string}
+            except:
+                resp_json = "none"
+        else:
+            print("not translating\n")
+            resp_json = {"ans": str(generated_text), "docs": doc_sources_string}
         return resp_json
 
-class CustomerExampleTool(Tool):
-    name = "customer_example_tool"
-    description = "Use this tool for any customer related question to help customers understand best practices on building on AWS. It will use the relevant context from the AWS Well-Architected Framework to answer the customer's query. The input is the customer's question. The tool returns an answer for the customer using the relevant context."
+class MyelomaFoundationTool(Tool):
+    name = "myeloma_foundation_tool"
+    description = "Use this tool for any Myeloma Foundation related question to help customers understand best practices on building on AWS. It will use the relevant context from the AWS Well-Architected Framework to answer the customer's query. The input is the customer's question. The tool returns an answer for the customer using the relevant context."
     inputs = ["text"]
     outputs = ["text"]
 
-    def __call__(self, query):
+    def __call__(self, query, translation_language):
         # Find docs
         embeddings = BedrockEmbeddings()
-        vectorstore = FAISS.load_local("local_index_customer", embeddings)
+        vectorstore = FAISS.load_local("local_index_myeloma_foundation", embeddings)
         docs = vectorstore.similarity_search(query)
         
         context = ""
@@ -97,10 +135,352 @@ class CustomerExampleTool(Tool):
         Question: {query}
         Answer:"""
 
-        generated_text = call_bedrock(prompt)
-        print(generated_text)
+        # print prompt for log
+        print("prompt:\n")
+        print(prompt)
+        print("\nend of prompt\n")
 
-        resp_json = {"ans": str(generated_text), "docs": doc_sources_string}
+        generated_text = call_bedrock(prompt)
+        #print(generated_text)
+        #translate
+        if translation_language:
+            print("translating\n")
+            translation_prompt = generated_text 
+            try:
+                translated_text = call_ai21(translation_prompt)
+                resp_json = {"ans": str(translated_text['prompt']['text']), "docs": doc_sources_string}
+            except:
+                resp_json = "none"
+        else:
+            print("not translating\n")
+            resp_json = {"ans": str(generated_text), "docs": doc_sources_string}
+        return resp_json
+class WhoWePlayForTool(Tool):
+    name = "who_we_play_for_tool"
+    description = "Use this tool for any Who We Play For related question to help customers understand best practices on building on AWS. It will use the relevant context from the AWS Well-Architected Framework to answer the customer's query. The input is the customer's question. The tool returns an answer for the customer using the relevant context."
+    inputs = ["text"]
+    outputs = ["text"]
+
+    def __call__(self, query, translation_language):
+        # Find docs
+        embeddings = BedrockEmbeddings()
+        vectorstore = FAISS.load_local("local_index_who_we_play_for", embeddings)
+        docs = vectorstore.similarity_search(query)
+        
+        context = ""
+
+        doc_sources_string = ""
+        for doc in docs:
+            doc_sources_string += doc.metadata["source"] + "\n"
+            context += doc.page_content
+
+        prompt = f"""Use the following pieces of context to answer the question at the end. Give a very detailed, long answer.
+
+        {context}
+
+        Question: {query}
+        Answer:"""
+
+        # print prompt for log
+        print("prompt:\n")
+        print(prompt)
+        print("\nend of prompt\n")
+
+        generated_text = call_bedrock(prompt)
+        #print(generated_text)
+        #translate
+        if translation_language:
+            print("translating\n")
+            translation_prompt = generated_text 
+            try:
+                translated_text = call_ai21(translation_prompt)
+                resp_json = {"ans": str(translated_text['prompt']['text']), "docs": doc_sources_string}
+            except:
+                resp_json = "none"
+        else:
+            print("not translating\n")
+            resp_json = {"ans": str(generated_text), "docs": doc_sources_string} 
+        return resp_json
+
+class BroadviewFCUTool(Tool):
+    name = "broadview_tool"
+    description = "Use this tool for any Broadview FCU related question to help customers understand best practices on building on AWS. It will use the relevant context from the AWS Well-Architected Framework to answer the customer's query. The input is the customer's question. The tool returns an answer for the customer using the relevant context."
+    inputs = ["text"]
+    outputs = ["text"]
+
+    def __call__(self, query, translation_language):
+        # Find docs
+        embeddings = BedrockEmbeddings()
+        vectorstore = FAISS.load_local("local_index_broadview_fcu", embeddings)
+        docs = vectorstore.similarity_search(query)
+        
+        context = ""
+
+        doc_sources_string = ""
+        for doc in docs:
+            doc_sources_string += doc.metadata["source"] + "\n"
+            context += doc.page_content
+
+        prompt = f"""Use the following pieces of context to answer the question at the end. Give a very detailed, long answer.
+
+        {context}
+
+        Question: {query}
+        Answer:"""
+
+        # print prompt for log
+        print("prompt:\n")
+        print(prompt)
+        print("\nend of prompt\n")
+
+        generated_text = call_bedrock(prompt)
+        #print(generated_text)
+        #translate
+        if translation_language:
+            print("translating\n")
+            translation_prompt = generated_text 
+            try:
+                translated_text = call_ai21(translation_prompt)
+                resp_json = {"ans": str(translated_text['prompt']['text']), "docs": doc_sources_string}
+            except:
+                resp_json = "none"
+        else:
+            print("not translating\n")
+            resp_json = {"ans": str(generated_text), "docs": doc_sources_string}
+        return resp_json
+    
+class TEDTool(Tool):
+    name = "ted_tool"
+    description = "Use this tool for any TED related question to help customers understand best practices on building on AWS. It will use the relevant context from the AWS Well-Architected Framework to answer the customer's query. The input is the customer's question. The tool returns an answer for the customer using the relevant context."
+    inputs = ["text"]
+    outputs = ["text"]
+
+    def __call__(self, query, translation_language):
+        # Find docs
+        embeddings = BedrockEmbeddings()
+        vectorstore = FAISS.load_local("local_index_ted", embeddings)
+        docs = vectorstore.similarity_search(query)
+        
+        context = ""
+
+        doc_sources_string = ""
+        for doc in docs:
+            doc_sources_string += doc.metadata["source"] + "\n"
+            context += doc.page_content
+
+        prompt = f"""Use the following pieces of context to answer the question at the end. Give a very detailed, long answer.
+
+        {context}
+
+        Question: {query}
+        Answer:"""
+
+        # print prompt for log
+        print("prompt:\n")
+        print(prompt)
+        print("\nend of prompt\n")
+        generated_text = call_bedrock(prompt)
+        # print(generated_text)
+        #translate
+        if translation_language:
+            print("translating\n")
+            translation_prompt = generated_text 
+            try:
+                translated_text = call_ai21(translation_prompt)
+                resp_json = {"ans": str(translated_text['prompt']['text']), "docs": doc_sources_string}
+            except:
+                resp_json = "none"
+        else:
+            print("not translating\n")
+            resp_json = {"ans": str(generated_text), "docs": doc_sources_string}
+        return resp_json
+
+class GettrTool(Tool):
+    name = "gettr_tool"
+    description = "Use this tool for any Gettr related question to help customers understand best practices on building on AWS. It will use the relevant context from the AWS Well-Architected Framework to answer the customer's query. The input is the customer's question. The tool returns an answer for the customer using the relevant context."
+    inputs = ["text"]
+    outputs = ["text"]
+
+    def __call__(self, query, translation_language):
+        # Find docs
+        embeddings = BedrockEmbeddings()
+        vectorstore = FAISS.load_local("local_index_gettr", embeddings)
+        docs = vectorstore.similarity_search(query)
+        
+        context = ""
+
+        doc_sources_string = ""
+        for doc in docs:
+            doc_sources_string += doc.metadata["source"] + "\n"
+            context += doc.page_content
+
+        prompt = f"""Use the following pieces of context to answer the question at the end. Give a very detailed, long answer.
+
+        {context}
+
+        Question: {query}
+        Answer:"""
+
+        # print prompt for log
+        print("prompt:\n")
+        print(prompt)
+        print("\nend of prompt\n")
+
+        generated_text = call_bedrock(prompt)
+        #print(generated_text)
+        #translate
+        if translation_language:
+            print("translating\n")
+            translation_prompt = generated_text 
+            try:
+                translated_text = call_ai21(translation_prompt)
+                resp_json = {"ans": str(translated_text['prompt']['text']), "docs": doc_sources_string}
+            except:
+                resp_json = "none"
+        else:
+            print("not translating\n")
+            resp_json = {"ans": str(generated_text), "docs": doc_sources_string}
+        return resp_json
+    
+class CK_12Tool(Tool):
+    name = "ck_12_tool"
+    description = "Use this tool for any CK-12 related question to help customers understand best practices on building on AWS. It will use the relevant context from the AWS Well-Architected Framework to answer the customer's query. The input is the customer's question. The tool returns an answer for the customer using the relevant context."
+    inputs = ["text"]
+    outputs = ["text"]
+
+    def __call__(self, query, translation_language):
+        # Find docs
+        embeddings = BedrockEmbeddings()
+        vectorstore = FAISS.load_local("local_index_ck_12", embeddings)
+        docs = vectorstore.similarity_search(query)
+        
+        context = ""
+
+        doc_sources_string = ""
+        for doc in docs:
+            doc_sources_string += doc.metadata["source"] + "\n"
+            context += doc.page_content
+
+        prompt = f"""Use the following pieces of context to answer the question at the end. Give a very detailed, long answer.
+
+        {context}
+
+        Question: {query}
+        Answer:"""
+
+        # print prompt for log
+        print("prompt:\n")
+        print(prompt)
+        print("\nend of prompt\n")
+
+        generated_text = call_bedrock(prompt)
+        #print(generated_text)
+        #translate
+        if translation_language:
+            print("translating\n")
+            translation_prompt = generated_text 
+            try:
+                translated_text = call_ai21(translation_prompt)
+                resp_json = {"ans": str(translated_text['prompt']['text']), "docs": doc_sources_string}
+            except:
+                resp_json = "none"
+        else:
+            print("not translating\n")
+            resp_json = {"ans": str(generated_text), "docs": doc_sources_string}
+        return resp_json
+    
+class CFATool(Tool):
+    name = "cfa_tool"
+    description = "Use this tool for any CFA related question to help customers understand best practices on building on AWS. It will use the relevant context from the AWS Well-Architected Framework to answer the customer's query. The input is the customer's question. The tool returns an answer for the customer using the relevant context."
+    inputs = ["text"]
+    outputs = ["text"]
+
+    def __call__(self, query, translation_language):
+        # Find docs
+        embeddings = BedrockEmbeddings()
+        vectorstore = FAISS.load_local("local_index_cfa_institute", embeddings)
+        docs = vectorstore.similarity_search(query)
+        
+        context = ""
+
+        doc_sources_string = ""
+        for doc in docs:
+            doc_sources_string += doc.metadata["source"] + "\n"
+            context += doc.page_content
+
+        prompt = f"""Use the following pieces of context to answer the question at the end. Give a very detailed, long answer.
+
+        {context}
+
+        Question: {query}
+        Answer:"""
+
+        # print prompt for log
+        print("prompt:\n")
+        print(prompt)
+        print("\nend of prompt\n")
+
+        generated_text = call_bedrock(prompt)
+        #print(generated_text)
+        #translate
+        if translation_language:
+            print("translating\n")
+            translation_prompt = generated_text 
+            try:
+                translated_text = call_ai21(translation_prompt)
+                resp_json = {"ans": str(translated_text['prompt']['text']), "docs": doc_sources_string}
+            except:
+                resp_json = "none"
+        else:
+            print("not translating\n")
+            resp_json = {"ans": str(generated_text), "docs": doc_sources_string}
+        return resp_json
+    
+class JehovasWitnessTool(Tool):
+    name = "jw_tool"
+    description = "Use this tool for any Jehova's Witness related question to help customers understand best practices on building on AWS. It will use the relevant context from the AWS Well-Architected Framework to answer the customer's query. The input is the customer's question. The tool returns an answer for the customer using the relevant context."
+    inputs = ["text"]
+    outputs = ["text"]
+
+    def __call__(self, query, translation_language):
+        # Find docs
+        embeddings = BedrockEmbeddings()
+        vectorstore = FAISS.load_local("local_index_jehovah_witness", embeddings)
+        docs = vectorstore.similarity_search(query)
+        
+        context = ""
+
+        doc_sources_string = ""
+        for doc in docs:
+            doc_sources_string += doc.metadata["source"] + "\n"
+            context += doc.page_content
+
+        prompt = f"""Use the following pieces of context to answer the question at the end. Give a very detailed, long answer.
+
+        {context}
+
+        Question: {query}
+        Answer:"""
+
+        # print prompt for log
+        #print("prompt:\n")
+        #print(prompt)
+        #print("\nend of prompt\n")
+
+        generated_text = call_bedrock(prompt)
+        #print(generated_text)
+        #translate
+        if translation_language:
+            print("translating\n")
+            translation_prompt = generated_text 
+            try:
+                translated_text = call_ai21(translation_prompt)
+                resp_json = {"ans": str(translated_text['prompt']['text']), "docs": doc_sources_string}
+            except:
+                resp_json = "none"
+        else:
+            print("not translating\n")
+            resp_json = {"ans": str(generated_text), "docs": doc_sources_string}
         return resp_json
 
 class CodeGenerationTool(Tool):
@@ -110,7 +490,7 @@ class CodeGenerationTool(Tool):
     inputs = ["text"]
     outputs = ["text"]
 
-    def __call__(self, prompt):
+    def __call__(self, prompt, translation_language):
         generated_text = call_bedrock(prompt)
         return generated_text
 
--- bedrock_tools_st.py
+++ bedrock_tools_st.py
@@ -1,15 +1,22 @@
 import json
-
+import sys
 import boto3
 from langchain.embeddings import BedrockEmbeddings
 from langchain.vectorstores import FAISS
 from transformers import Tool
 import streamlit as st
-from bedrock_tools import AWSWellArchTool, CodeGenerationTool, CustomerExampleTool
+from bedrock_tools import AWSWellArchTool, CodeGenerationTool, GettrTool, CK_12Tool, CFATool, JehovasWitnessTool, BroadviewFCUTool, TEDTool, MyelomaFoundationTool, WhoWePlayForTool
 
 well_arch_tool = AWSWellArchTool()
 code_gen_tool = CodeGenerationTool()
-customer_example_tool = CustomerExampleTool()
+gettr_tool = GettrTool()
+ck_12_tool = CK_12Tool()
+cfa_tool = CFATool()
+jw_tool = JehovasWitnessTool()
+broadview_tool = BroadviewFCUTool()
+ted_tool = TEDTool()
+myeloma_foundation_tool = MyelomaFoundationTool()
+who_we_play_for_tool = WhoWePlayForTool()
 
 
 bedrock = boto3.client(
@@ -32,7 +39,7 @@ def call_bedrock(prompt):
 
     body = json.dumps(prompt_config)
 
-    modelId = "amazon.titan-tg1-large"
+    modelId = "amazon.titan-tg1-large" #"ai21.j2-ultra" 
     accept = "application/json"
     contentType = "application/json"
 
@@ -61,7 +68,7 @@ def call_bedrock(prompt):
 # output = code_gen_tool(query)
 # print(output)
 
-def app() -> None:
+def app(multilingual) -> None:
     """
     Purpose:
         Controls the app flow
@@ -73,20 +80,39 @@ def app() -> None:
 
     # Choose tool
     current_tool = st.selectbox(
-        "Choose Tool:", ["AWS Well Architected Tool", "Code Generation Tool", "Customer Q&A"]
+        "Choose Tool:", ["AWS Well Architected Tool", "Code Generation Tool", "Broadview FCU Q&A", "Myeloma Foundation Q&A", "TED Q&A", "Gettr Q&A", "CK-12 Q&A", "Jehova's Witness Q&A", "CFA Institute Q&A", "Who We Play For Q&A"]
     )
 
     query = st.text_input("Query:")
 
+    #translation_language = st.selectbox(
+    #    "Choose Language:", ["English", "French", "Spanish", "German", "Dutch", "Italian", "Arabic", "Bengali", "Esperanto"]
+    #)
+    translation_language = multilingual
+
     if st.button("Submit Query"):
         with st.spinner("Generating..."):
             if current_tool == "AWS Well Architected Tool":
-                answer = well_arch_tool(query)
-            elif current_tool == "Customer Q&A":
-                answer = customer_example_tool(query)
+                answer = well_arch_tool(query, translation_language)
+            elif current_tool == "Broadview FCU Q&A":
+                answer = broadview_tool(query, translation_language)
+            elif current_tool == "TED Q&A":
+                answer = ted_tool(query, translation_language)
+            elif current_tool == "Gettr Q&A":
+                answer = gettr_tool(query, translation_language)
+            elif current_tool == "CK-12 Q&A":
+                answer = ck_12_tool(query, translation_language)
+            elif current_tool == "Jehova's Witness Q&A":
+                answer = jw_tool(query, translation_language)
+            elif current_tool == "CFA Institute Q&A":
+                answer = cfa_tool(query, translation_language)
+            elif current_tool == "Myeloma Foundation Q&A":
+                answer = myeloma_foundation_tool(query, translation_language)
+            elif current_tool == "Who We Play For Q&A":
+                answer = who_we_play_for_tool(query, translation_language)
             elif current_tool == "Code Generation Tool":
                 print("codegen")
-                answer = code_gen_tool(query)
+                answer = code_gen_tool(query, translation_language)
 
             if type(answer) == dict:
                 st.markdown(answer["ans"])
@@ -99,7 +125,7 @@ def app() -> None:
                 st.markdown(answer)
 
 
-def main() -> None:
+def main(multilingual) -> None:
     """
     Purpose:
         Controls the flow of the streamlit app
@@ -112,8 +138,10 @@ def main() -> None:
     # Start the streamlit app
     st.title("Bedrock Q&A")
 
-    app()
+    app(multilingual)
 
 
 if __name__ == "__main__":
-    main()
+    multilingual = bool(int(sys.argv[1]))
+    print('multilingual: ' + str(multilingual))
+    main(multilingual)
